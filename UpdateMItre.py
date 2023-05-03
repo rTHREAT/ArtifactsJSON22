cbb1831e58c60ef327ac4fbb3a4de2c904e52d1a
MikaelRex <miguel.juarez@rthreat.com>
2023-05-03 11:39:39 -0600

    030523

--- UpdateMItre.py
+++ UpdateMItre.py
@@ -6,8 +6,8 @@ for filename in glob.glob('*.json'):
     data = json.load(jsonFile) # Read the JSON into the buffer
     jsonFile.close() # Close the JSON file
 
-    data["versions"]["attack"]  = "12"
-    data["versions"]["navigator"]  = "4.8.0"
+    data["versions"]["attack"]  = "13"
+    data["versions"]["navigator"]  = "4.8.1"
     data["versions"]["layer"]  = "4.4"
 
     jsonFile = open(filename, "w")
